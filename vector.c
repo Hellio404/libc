ac116c932aa8d06ec215d604e99e45354898829a
Youness FARINI <farini.work@gmail.com>
2022-08-21 11:33:34 +0100

    fix the norm issues

--- vector.c
+++ vector.c
@@ -1,111 +1,167 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vector.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yfarini <yfarini@student.1337.ma>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/21 10:31:38 by yfarini           #+#    #+#             */
+/*   Updated: 2022/08/21 10:50:34 by yfarini          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <stddef.h>
 #include <string.h>
 #include "vector.h"
 
-vector_t    *v_create(size_t size) {
+t_vector	*v_create(size_t size)
+{
+	t_vector	*ptr;
 
-    vector_t *ptr;
-
-    ptr = malloc(sizeof(vector_t));
-    if (!ptr) return NULL;
-
-    ptr->_end = NULL;
-    ptr->_begin = NULL;
-    ptr->_end_cap = NULL;
-    ptr->_size = size;
-    return ptr;
+	ptr = malloc(sizeof(t_vector));
+	if (!ptr)
+		return (NULL);
+	ptr->_end = NULL;
+	ptr->_begin = NULL;
+	ptr->_end_cap = NULL;
+	ptr->_size = size;
+	return (ptr);
 }
 
-void    *v_push(vector_t *vt) {
+void	*v_push(t_vector *vt)
+{
+	size_t	new_size;
+	void	*ret;
 
-    if (vt->_end + vt->_size > vt->_end_cap) {
-        size_t new_size = (((unsigned char*)vt->_end - (unsigned char *)vt->_begin) / vt->_size) * 2;
-        if (new_size == 0)
-            new_size = 1;
-        if (v_reserve(vt, new_size))
-            return NULL;
-    }
-    void *ret = vt->_end;
-    vt->_end += vt->_size;
-    return ret;
+	if (vt->_end + vt->_size > vt->_end_cap)
+	{
+		new_size = (((unsigned char *)vt->_end
+					- (unsigned char *)vt->_begin) / vt->_size) * 2;
+		if (new_size == 0)
+			new_size = 1;
+		if (v_reserve(vt, new_size))
+			return (NULL);
+	}
+	ret = vt->_end;
+	vt->_end += vt->_size;
+	return (ret);
 }
 
-void v_pop(vector_t *vt, void (*fn) (void *)) {
-    if (vt->_end <= vt->_begin) 
-        return ;
-    
-    if (fn)
-        fn(--vt->_end);
-    else
-        --vt->_end;
+void	v_pop(t_vector *vt, void (*fn)(void *))
+{
+	if (vt->_end <= vt->_begin)
+		return ;
+	if (fn)
+		fn(--vt->_end);
+	else
+		--vt->_end;
 }
 
-int    v_reserve(vector_t *vt, size_t size) {
-    if (v_capacity(vt) >= size) return 0;
-    size_t new_size = size * vt->_size;
-    if (new_size == 0)
-        new_size = 1;
-    void *replace = malloc(new_size * vt->_size);
-    if (!replace)
-        return 1;
-    memcpy(replace, vt->_begin, ((unsigned char*)vt->_end - (unsigned char *)vt->_begin));
-    free(vt->_begin);
-    vt->_end_cap = replace + new_size * vt->_size;
-    vt->_end = replace + ((unsigned char*)vt->_end - (unsigned char *)vt->_begin);
-    vt->_begin = replace;
-    return 0;
+int	v_reserve(t_vector *vt, size_t size)
+{
+	size_t	new_size;
+	void	*replace;
+
+	if (v_capacity(vt) >= size)
+		return (0);
+	new_size = size * vt->_size;
+	if (new_size == 0)
+		new_size = 1;
+	replace = malloc(new_size * vt->_size);
+	if (!replace)
+		return (1);
+	memcpy(replace, vt->_begin,
+		((unsigned char *)vt->_end - (unsigned char *)vt->_begin));
+	free(vt->_begin);
+	vt->_end_cap = replace + new_size * vt->_size;
+	vt->_end = replace + ((unsigned char *)vt->_end
+			- (unsigned char *)vt->_begin);
+	vt->_begin = replace;
+	return (0);
 }
 
-int v_resize(vector_t *vt, size_t size, const void *default_value, void (*fn) (void *)) {
-    while (v_size(vt) > size)
-        v_pop(vt, fn);
-    if (v_size(vt) < size) {
-        if (v_reserve(vt, size))
-            return 1;
-        size_t diff =  size - v_size(vt);
-        for (size_t i = 0; i < diff; ++i) {
-            memmove(vt->_end, default_value, vt->_size);
-            vt->_end += vt->_size;
-        }
-    }
-    return 0;
+int	v_resize(
+	t_vector *vt,
+	size_t size,
+	const void *default_value,
+	void (*fn) (void *))
+{
+	size_t	diff;
+	size_t	i;
+
+	while (v_size(vt) > size)
+		v_pop(vt, fn);
+	if (v_size(vt) < size)
+	{
+		if (v_reserve(vt, size))
+			return (1);
+		diff = size - v_size(vt);
+		while (i < diff)
+		{
+			memmove(vt->_end, default_value, vt->_size);
+			vt->_end += vt->_size;
+			++i;
+		}
+	}
+	return (0);
 }
 
-size_t    v_size(vector_t *vt) {
-
-    return (((unsigned char*)vt->_end - (unsigned char *)vt->_begin) / vt->_size);
+size_t	v_size(t_vector *vt)
+{
+	return (
+		((unsigned char*)vt->_end - (unsigned char *)vt->_begin)
+		/ vt->_size
+	);
 }
 
-size_t    v_capacity(vector_t *vt) {
-
-    return (((unsigned char*)vt->_end_cap - (unsigned char *)vt->_begin) / vt->_size);
+size_t	v_capacity(t_vector *vt)
+{
+	return (
+		((unsigned char*)vt->_end_cap - (unsigned char *)vt->_begin)
+		/ vt->_size
+	);
 }
 
-int         v_empty(vector_t *vt) {
-
-    return v_size(vt) == 0;
+int	v_empty(t_vector *vt)
+{
+	return (v_size(vt) == 0);
 }
 
-void *v_begin(vector_t *vt) {
-    return vt->_begin;
+void	*v_begin(t_vector *vt)
+{
+	return (vt->_begin);
 }
 
-void *v_end(vector_t *vt) {
-    return vt->_end;
+void	*v_end(t_vector *vt)
+{
+	return (vt->_end);
 }
 
-void v_clear(vector_t *vt, void (*fn) (void *)) {
-    if (fn) {
-        for (void *b = vt->_begin; b != vt->_end; b += vt->_size)
-            fn(b);
-    }
-    free(vt->_begin);
-    vt->_begin = vt->_end = vt->_end_cap = NULL;
+void	v_clear(t_vector *vt, void (*fn) (void *))
+{
+	void	*begin;
+
+	if (fn)
+	{
+		begin = vt->_begin;
+		while (begin != vt->_end)
+		{
+			fn(begin);
+			begin += vt->_size;
+		}
+	}
+	free(vt->_begin);
+	vt->_begin = NULL;
+	vt->_end = NULL;
+	vt->_end_cap = NULL;
 }
 
-void       v_destroy(vector_t *vt, void (*fn) (void *)) {
-    v_clear(vt, fn);
-    free(vt);
+void	v_destroy(t_vector *vt, void (*fn) (void *))
+{
+	if (!vt)
+		return ;
+	v_clear(vt, fn);
+	free(vt);
 }
--- vector.h
+++ vector.h
@@ -1,24 +1,37 @@
-#ifndef VECTOR_H
-#define VECTOR_H
-#include <stdlib.h>
-#include <stdio.h>
-#include <stddef.h>
-#include <string.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vector.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yfarini <yfarini@student.1337.ma>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/21 10:47:36 by yfarini           #+#    #+#             */
+/*   Updated: 2022/08/21 10:55:31 by yfarini          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-typedef struct {
-    void*   _end_cap;
-    void*   _end;
-    size_t  _size;
-    void*   _begin;
-} vector_t;
+#ifndef VECTOR_H
+# define VECTOR_H
+# include <stdlib.h>
+# include <stdio.h>
+# include <stddef.h>
+# include <string.h>
+
+typedef struct s_vector
+{
+	void	*_end_cap;
+	void	*_end;
+	size_t	_size;
+	void	*_begin;
+}				t_vector;
 
 /**
  * @brief create a vector with 0 elements of size 'size'
  * 
  * @param size the size of the type that will be inserted to the vector
- * @return vector_t* the vector pointer (NULL in case of failure)
+ * @return t_vector* the vector pointer (NULL in case of failure)
  */
-vector_t    *v_create(size_t size);
+t_vector	*v_create(size_t size);
 
 /**
  * @brief push the end of the vector by one and return a pointer to the old end
@@ -26,7 +39,7 @@ vector_t    *v_create(size_t size);
  * @param vt the vector pointer
  * @return void* pointer to the element before the end
  */
-void        *v_push(vector_t *vt);
+void		*v_push(t_vector *vt);
 
 /**
  * @brief remove an element from the end of the vector
@@ -34,29 +47,39 @@ void        *v_push(vector_t *vt);
  * @param vt the vector pointer
  * @param fn a function to run on the last element before removing it (eg: free)
  */
-void        v_pop(vector_t *vt, void (*fn) (void *));
+void		v_pop(t_vector *vt, void (*fn) (void *));
 
 /**
- * @brief reserve the capacity size on the vector. On success is guaranteed that the capacity of the vector will be >= size
+ * @brief reserve the capacity size on the vector.
+ * On success is guaranteed that the capacity of the vector will be >= size
  * 
  * @param vt the vector pointer
  * @param size the capacity to reserve
  * @return int 
  */
-int         v_reserve(vector_t *vt, size_t size);
+int			v_reserve(t_vector *vt, size_t size);
 
 /**
- * @brief resize the vector to be exactly "size" if the vector has more elements than "size" 
- * all the additional elements will be removed form the end of the vector.
- * If the vector has less elements than "size", values will be added to the end of the vector using the "default_value"
+ * 
+ * @brief resize the vector to be exactly "size" if the vector has more
+ * elements than "size" all the additional elements will be removed
+ * form the end of the vector. If the vector has less elements than "size",
+ * values will be added to the end of the vector using the "default_value"
  * 
  * @param vt the vector pointer
  * @param size the target size of the vector
- * @param default_value a pointer to the default value used to fill the needed elements
- * @param fn a function that will be called before the any element destruction OR NULL if no action needed
+ * @param default_value a pointer to the default value used to
+ * 						fill the needed elements
+ * @param fn a function that will be called before the any element destruction
+ * 			OR NULL if no action needed
  * @return int 0 on success and 1 otherwise
  */
-int         v_resize(vector_t *vt, size_t size, const void *default_value, void (*fn) (void *));
+int			v_resize(
+				t_vector *vt,
+				size_t size,
+				const void *default_value,
+				void (*fn) (void *)
+				);
 
 /**
  * @brief return the current size of the vector
@@ -64,7 +87,7 @@ int         v_resize(vector_t *vt, size_t size, const void *default_value, void 
  * @param vt the vector pointer
  * @return size_t the size of the vector
  */
-size_t      v_size(vector_t *vt);
+size_t		v_size(t_vector *vt);
 
 /**
  * @brief return the current capacity of the vector
@@ -72,15 +95,16 @@ size_t      v_size(vector_t *vt);
  * @param vt the vector pointer
  * @return size_t the capacity of the vector
  */
-size_t      v_capacity(vector_t *vt);
+size_t		v_capacity(t_vector *vt);
 
 /**
- * @brief return whather the vector is empty or not (1 if it's empty and 0 if it's not)
+ * @brief return whather the vector is empty
+ * 	or not (1 if it's empty and 0 if it's not)
  * 
  * @param vt the vector pointer
  * @return int 1 if the vector is empty and 0 if it's not
  */
-int         v_empty(vector_t *vt);
+int			v_empty(t_vector *vt);
 
 /**
  * @brief return a pointer to the beginning of the data hold by the vector
@@ -88,7 +112,7 @@ int         v_empty(vector_t *vt);
  * @param vt the vector pointer
  * @return void* the beginning
  */
-void        *v_begin(vector_t *vt);
+void		*v_begin(t_vector *vt);
 
 /**
  * @brief return a pointer to the past the end of the data hold by the vector
@@ -96,21 +120,23 @@ void        *v_begin(vector_t *vt);
  * @param vt the vector pointer
  * @return void* the end of the vector
  */
-void        *v_end(vector_t *vt);
+void		*v_end(t_vector *vt);
 
 /**
  * @brief remove all the elements on vector
  * 
  * @param vt the vector pointer
- * @param fn a function to run before removing each element. it can be NULL if no action needed
+ * @param fn a function to run before removing each element.
+ * 			it can be NULL if no action needed
  */
-void        v_clear(vector_t *vt, void (*fn) (void *));
+void		v_clear(t_vector *vt, void (*fn) (void *));
 
 /**
  * @brief remove and free all the resources allocated by the vector
  * 
  * @param vt the vector pointer
- * @param fn a function to run before removing each element. it can be NULL if no action needed
+ * @param fn a function to run before removing each element.
+ * 			it can be NULL if no action needed
  */
-void        v_destroy(vector_t *vt, void (*fn) (void *));
+void		v_destroy(t_vector *vt, void (*fn) (void *));
 #endif
